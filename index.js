10fd21e0f126f85e392c11a88b452cedf7b4e37f
Temamr <blazingbass@mail.ru>
2026-03-10 17:43:46 +0500

    showAndHide added

--- index.js
+++ index.js
@@ -25,41 +25,14 @@ function addListeners() {
             const block = document.getElementById('scaleBlock');
             animObj.scale(block, 1000, 1.25);
         });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animObj.showAndHide(block, 5000);
+        });
+
 }
-
-// /**
-//  * Блок плавно появляется из прозрачного.
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  */
-// function fadeIn(element, duration) {
-//     element.style.transitionDuration =  `${duration}ms`;
-//     element.classList.remove('hide');
-//     element.classList.add('show');
-// }
-
-// /**
-//  * Функция, передвигающая элемент
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  * @param translation — объект с полями x и y, обозначающими смещение блока
-//  */
-// function move(element, duration, translation) {
-//     element.style.transitionDuration = `${duration}ms`;
-//     element.style.transform = getTransform(translation, null);
-// }
-
-// /**
-//  * Функция, увеличивающая/уменьшающая элемент
-//  * @param element — HTMLElement, который надо анимировать
-//  * @param duration — Продолжительность анимации в миллисекундах
-//  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-//  */
-// function scale(element, duration, ratio) {
-//     element.style.transitionDuration =  `${duration}ms`;
-//     element.style.transform = getTransform(null, ratio);
-// }
-
 
 function getTransform(translation, ratio) {
     const result = [];
@@ -92,6 +65,10 @@ function animaster(){
             element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('show');
             element.classList.add('hide');
+        },
+        showAndHide: function(element, duration){
+            this.fadeIn(element, duration / 3)
+            setTimeout(() => this.fadeOut(element, duration / 3),duration / 3);
         }
     }
 }
